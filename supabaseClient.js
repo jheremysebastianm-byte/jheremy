ef1305197c3c5372a4799133bea05561914ce0bf
jheremysebastianm-byte <jheremysebastianm@gmail.com>
2026-08-13 18:38:44 -0400

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -7,7 +7,7 @@
 // 4. Copia "anon public key" y pégalo en SUPABASE_ANON_KEY
 // =========================================================
 (function () {
-const SUPABASE_URL = "https://ntmbcbgvanwgmxjylfij.supabase.co";
+const SUPABASE_URL = "sb_publishable_gYLXokPh0FSNdgu34Lqq1g_fVoNGv06";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im50bWJjYmd2YW53Z214anlsZmlqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU1MjAzNDgsImV4cCI6MjEwMTA5NjM0OH0.asya7vihtVfWUc-KEBlNjHSkYfr3sEx1hr86ARL_g6U";
 
 window.supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
